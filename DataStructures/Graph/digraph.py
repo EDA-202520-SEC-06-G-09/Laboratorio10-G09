a1cc7f2ee35c1de84f126753a592cbebd3b7155e
mgabrielah <mg.herrera@uniandes.edu.co>
2025-11-21 11:48:40 -0500

    Implementaciones y Correcciones funciones diagraph

--- DataStructures/Graph/digraph.py
+++ DataStructures/Graph/digraph.py
@@ -34,31 +34,46 @@ def update_vertex_info(my_graph, key_u, new_info):
     
     return my_graph
 
-def add_edge():
-    pass
-def contains_vertex():
-    pass
+def add_edge(my_graph, key_u, key_v, weight=1):
+    
+    if not mlp.contains(my_graph["vertices"], key_u):
+        raise Exception("El vertice u no existe")
+
+    if not mlp.contains(my_graph["vertices"], key_v):
+        raise Exception("El vertice v no existe")
+
+    vert_u = mlp.get(my_graph["vertices"], key_u)
+    vert_v = mlp.get(my_graph["vertices"], key_v)
+
+    adj_u = vert_u["adjacents"]
+
+    if mlp.contains(adj_u, key_v):
+        edge = mlp.get(adj_u, key_v)
+        edge["weight"] = weight
+    else:
+        new_edge = e.new_edge(key_v, weight)
+        mlp.put(adj_u, key_v, new_edge)
+        my_graph["num_edges"] += 1
+
+    return my_graph
+
+def contains_vertex(my_graph, key_u):
+   return mlp.contains(my_graph["vertices"], key_u)
 
 def order(my_graph): #Numero de nodos del grafo
-    order = mlp.size(my_graph)
+    order = mlp.size(my_graph['vertices'])
     return order
 
-def size(my_graph):#numero de arcos del grafo
-    vertices = mlp.key_set(my_graph)
-    s = 0
-    for llave in vertices:
-        vertice = mlp.get(my_graph,llave)
-        adyacentes = v.get_adjacents(vertice) #mapa de adyacentes
-        s += mlp.size(adyacentes)
-    return s
+def size(my_graph):
+    return my_graph["num_edges"]
 
 def degree(my_graph, llave): #Numero de arcos adyacentes al vertice 
-    vertice = mlp.get(my_graph, llave)
+    vertice = mlp.get(my_graph['vertices'], llave)
     degree = v.degree(vertice)
     return degree
 
 def adjacents(my_graph, key_u):
-    vertice = mlp.get(my_graph, key_u)
+    vertice = mlp.get(my_graph['vertices'], key_u)
     adyacentes = v.get_adjacents(vertice)
     lista = mlp.key_set(adyacentes)
     
@@ -66,29 +81,27 @@ def adjacents(my_graph, key_u):
 
 
 def vertices(my_graph):
-    lista = mlp.key_set(my_graph)
+    lista = mlp.key_set(my_graph['vertices'])
     
     return lista
 
 
-
-def get_vertex(my_graph, vid):
-        if vid not in my_graph["vertices"]:
-            return None
-        else:
-            return my_graph["vertices"][vid]["info"]
-            
-
-def update_vertex_info(my_graph, vid, new_info):
-    if vid not in my_graph["vertices"]:
-        return False
-    else:
-        return my_graph["vertices"][vid]["new_info"]
+def get_vertex(my_graph, key_u):
+    vert = mlp.get(my_graph["vertices"], key_u)
+    
+    if vert is None:
+        raise Exception("El vertice no existe")
+    
+    return vert
     
     
-def get_vertex_information (my_graph, vid):
-    if vid not in my_graph["vertices"]:
-        return None
-    else:
-        return my_graph["vertices"][vid]["info"]
+def get_vertex_information(my_graph, key_u):
+
+    if not contains_vertex(my_graph, key_u):
+        raise Exception("El vertice no existe")
+
+    vertice = mlp.get(my_graph["vertices"], key_u)
+
+    return vertice["value"]
+
     
